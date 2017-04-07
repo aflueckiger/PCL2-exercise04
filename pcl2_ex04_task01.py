aee1d7eb820034a55da64fe5e756c079483266e3
Alex Flückiger <alex.flueckiger@gmail.com>
2017-04-07 13:16:36 +0200

    create file with boilerplates for PCL2, exercise 04, task 01

---
+++ pcl2_ex04_task01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# PCL II, exercise 04, spring 2017
+# Task 1
+# Authors: Nora Lötscher & Alex Flückiger
+# Student-ID.: 09-110-552 & 12-452-223
+
+"""
+Module Description
+"""
+
+
+def getfreqwords(indir, outfile):
+    pass
+
+
+def main():
+    """
+    Call functions to test script.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
