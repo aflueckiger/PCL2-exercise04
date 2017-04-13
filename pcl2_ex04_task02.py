12927bfb9a85f4be287b7a960fd104f5c189b6c0
Alex Flückiger <alex.flueckiger@gmail.com>
2017-04-13 17:23:18 +0200

    created file with boilerplate for task 2, exercise 4, PCL2

---
+++ pcl2_ex04_task02.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# PCL II, exercise 04, spring 2017
+# Task 2
+# Authors: Nora Lötscher & Alex Flückiger
+# Student-ID.: 09-110-552 & 12-452-223
+
+"""
+Module Description XXXX
+"""
+
+
+def gettitles(infile, testfile, trainfile, k):
+    pass
+    
+def main():
+    """
+    Call functions to test script.
+    """
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
